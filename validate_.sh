470dcec9a411cecf31582372a58d1f800d9f7c50
Douglas Martin <mdgeek1@gmail.com>
2023-10-10 17:03:36 -0400

    Use new webjar validator plugin.

--- validate_.sh
+++
@@ -1,8 +0,0 @@
-file_exists "import-map.json"
-file_exists "README.md"
-file_contains "LICENSE" "Ajax.org B.V."
-
-dir_exists "dist/snippets"
-file_exists "dist/ace.js"
-file_exists "dist/mode-json.js"
-file_exists "dist/mode-xml.js"
